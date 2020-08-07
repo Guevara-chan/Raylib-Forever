f3f333bf882e3fd7e4dc698a0e887bfd5f6e2205
Victoria Guevara <Guevara-chan@mail.ru>
2020-08-07 19:19:02 +0300

    Add files via upload

--- examples/core/camera_2d.nim
+++ examples/core/camera_2d.nim
@@ -12,34 +12,35 @@
 
 import raylib
 
-const MAX_BUILDINGS = 100
-
-const screenWidth: int = 800
-const screenHeight: int = 450
+const 
+    MAX_BUILDINGS = 100
+    screenWidth   = 800
+    screenHeight  = 450
 
 InitWindow(screenWidth, screenHeight, "Camera 2D")
 
-var player : Rectangle = Rectangle(x: 400, y: 280, width: 40, height: 40)
-var buildings: array[MAX_BUILDINGS, Rectangle]
-var buildColors: array[MAX_BUILDINGS, Color]
-
-var spacing : int = 0
+var 
+    player  = Rectangle(x: 400, y: 280, width: 40, height: 40)
+    buildings:   array[MAX_BUILDINGS, Rectangle]
+    buildColors: array[MAX_BUILDINGS, Color]
+    spacing = 0
 
 for i in 0..<MAX_BUILDINGS:
-    buildings[i].width = float(GetRandomValue(50, 200))
-    buildings[i].height = float(GetRandomValue(100, 800))
+    buildings[i].width  = GetRandomValue(50, 200).float
+    buildings[i].height = GetRandomValue(100, 800).float
     buildings[i].y = screenHeight - 130 - buildings[i].height
-    buildings[i].x = -6000 + float(spacing)
+    buildings[i].x = -6000 + spacing.float
 
-    spacing += int(buildings[i].width)
+    spacing += buildings[i].width.int
 
-    buildColors[i] = Color(r:uint8(GetRandomValue(200,240)), g:uint8(GetRandomValue(200,240)), b:uint8(GetRandomValue(200,240)), a: 255)
+    buildColors[i] = Color(r: GetRandomValue(200, 240).uint8, g: GetRandomValue(200, 240).uint8, 
+        b: GetRandomValue(200, 240).uint8, a: 255)
     #buildColors[i] = RED
-var camera : Camera2D = Camera2D()
-camera.target = Vector2(x:player.x + 20,y: player.y + 20)
-camera.offset = Vector2(x:screenWidth/2,y: screenHeight/2)
-camera.rotation = 0.0f
-camera.zoom = 1.0f
+var camera = Camera2D()
+camera.target = Vector2(x: player.x + 20, y: player.y + 20)
+camera.offset = Vector2(x: screenWidth / 2, y: screenHeight / 2)
+camera.rotation = 0.0
+camera.zoom = 1.0
 
 60.SetTargetFPS
 
@@ -48,48 +49,48 @@ while not WindowShouldClose():
     #----------------------------------------------------------------------------------
     
     # Player movement
-    if IsKeyDown(KEY_RIGHT): player.x += 2
+    if IsKeyDown(KEY_RIGHT):  player.x += 2
     elif IsKeyDown(KEY_LEFT): player.x -= 2
     # Camera target follows player
     camera.target = Vector2(x: player.x + 20,y: player.y + 20)
     # Camera rotation controls
-    if IsKeyDown(KEY_A): camera.rotation = camera.rotation - 1
-    elif IsKeyDown(KEY_S): camera.rotation = camera.rotation + 1
+    if IsKeyDown(KEY_A):   camera.rotation -= 1
+    elif IsKeyDown(KEY_S): camera.rotation += 1
     # Limit camera rotation to 80 degrees (-40 to 40)
-    if camera.rotation > 40: camera.rotation = 40
+    if camera.rotation > 40:    camera.rotation = 40
     elif camera.rotation < -40: camera.rotation = -40
     # Camera zoom controls
-    camera.zoom += (float(GetMouseWheelMove())*0.05f)
-    if camera.zoom > 3.0f: camera.zoom = 3.0f
-    elif camera.zoom < 0.1f: camera.zoom = 0.1f
+    camera.zoom += GetMouseWheelMove().float * 0.05
+    if camera.zoom > 3.0:   camera.zoom = 3.0
+    elif camera.zoom < 0.1: camera.zoom = 0.1
     # Camera reset (zoom and rotation)
     if IsKeyPressed(KEY_R):
-        camera.zoom = 1.0f
-        camera.rotation = 0.0f
+        camera.zoom     = 1.0
+        camera.rotation = 0.0
     #----------------------------------------------------------------------------------
     # Draw
     #----------------------------------------------------------------------------------
     BeginDrawing()
-    ClearBackground(RAYWHITE)
-    BeginMode2D(camera)
-    DrawRectangle(-6000, 320, 13000, 8000, DARKGRAY)
+    ClearBackground RAYWHITE
+    BeginMode2D camera 
+    DrawRectangle -6000, 320, 13000, 8000, DARKGRAY
     for i in 0..<MAX_BUILDINGS: 
-        DrawRectangleRec(buildings[i], buildColors[i])
-    DrawRectangleRec(player, RED)
-    DrawLine(int(camera.target.x), -screenHeight*10, int(camera.target.x), screenHeight*10, GREEN)
-    DrawLine(-screenWidth*10, int(camera.target.y), screenWidth*10, int(camera.target.y), GREEN)
+        DrawRectangleRec buildings[i], buildColors[i]
+    DrawRectangleRec player, RED
+    DrawLine camera.target.x.int, -screenHeight * 10, camera.target.x.int, screenHeight * 10, GREEN
+    DrawLine -screenWidth * 10, camera.target.y.int, screenWidth * 10, camera.target.y.int, GREEN
     EndMode2D()
-    DrawText("SCREEN AREA", 640, 10, 20, RED)
-    DrawRectangle(0, 0, screenWidth, 5, RED)
-    DrawRectangle(0, 5, 5, screenHeight - 10, RED)
-    DrawRectangle(screenWidth - 5, 5, 5, screenHeight - 10, RED)
-    DrawRectangle(0, screenHeight - 5, screenWidth, 5, RED)
-    DrawRectangle( 10, 10, 250, 113, Fade(SKYBLUE, 0.5f))
-    DrawRectangleLines( 10, 10, 250, 113, BLUE)
-    DrawText("Free 2d camera controls:", 20, 20, 10, BLACK)
-    DrawText("- Right/Left to move Offset", 40, 40, 10, DARKGRAY)
-    DrawText("- Mouse Wheel to Zoom in-out", 40, 60, 10, DARKGRAY)
-    DrawText("- A / S to Rotate", 40, 80, 10, DARKGRAY)
-    DrawText("- R to reset Zoom and Rotation", 40, 100, 10, DARKGRAY)
+    DrawText "SCREEN AREA", 640, 10, 20, RED
+    DrawRectangle 0, 0, screenWidth, 5, RED
+    DrawRectangle 0, 5, 5, screenHeight - 10, RED
+    DrawRectangle screenWidth - 5, 5, 5, screenHeight - 10, RED
+    DrawRectangle 0, screenHeight - 5, screenWidth, 5, RED
+    DrawRectangle 10, 10, 250, 113, Fade(SKYBLUE, 0.5)
+    DrawRectangleLines 10, 10, 250, 113, BLUE
+    DrawText "Free 2d camera controls:", 20, 20, 10, BLACK
+    DrawText "- Right/Left to move Offset", 40, 40, 10, DARKGRAY
+    DrawText "- Mouse Wheel to Zoom in-out", 40, 60, 10, DARKGRAY
+    DrawText "- A / S to Rotate", 40, 80, 10, DARKGRAY
+    DrawText "- R to reset Zoom and Rotation", 40, 100, 10, DARKGRAY
     EndDrawing()
 CloseWindow()
